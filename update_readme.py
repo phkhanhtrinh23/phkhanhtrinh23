5aa29078a866d618b5ebbe907ea50b718acc9336
Trinh Pham <57525136+phkhanhtrinh23@users.noreply.github.com>
2024-10-15 16:34:21 +0700

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -12,10 +12,16 @@ print(f"Total Forks: {total_forks}")
 
 insert_str = f"\n![Total forks](https://img.shields.io/badge/Total%20forks-{total_forks}-brightgreen)\n"
 original_file = open("README.md", "r").readlines()
+
 for idx, line in enumerate(original_file):
     if line.startswith("<img src="):
+        if original_file[idx+2].startswith("![Total forks]"):
+            original_file[idx+2] = insert_str[1:] # skip the new line
         break
-original_file = original_file[:idx+1] + [insert_str] + original_file[idx+1:]
+
+if insert_str not in original_file and insert_str[1:] not in original_file:
+    original_file = original_file[:idx+1] + [insert_str] + original_file[idx+1:]
+
 original_file = "".join(original_file)
 
 with open("README.md", "w") as f:
